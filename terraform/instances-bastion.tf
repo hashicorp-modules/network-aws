fad13881562e076a0debd13e17f76c6eb691c401
Andrew Khoury <akhoury@live.com>
2017-03-14 17:12:56 -0500

    Refactored SSH key out

--- terraform/instances-bastion.tf
+++ terraform/instances-bastion.tf
@@ -1,9 +1,14 @@
+module "ssh_key" {
+  source = "git@github.com:hashicorp-modules/ssh-keypair.git//terraform"
+  environment_name = "${var.environment_name}"
+}
+
 resource "aws_instance" "bastion" {
   count = "${length(var.vpc_cidrs_public)}"
 
   ami           = "${data.aws_ami.main.image_id}"
   instance_type = "${var.bastion_instance_type}"
-  key_name      = "${aws_key_pair.main.key_name}"
+  key_name      = "${module.ssh_key.key_name}"
   subnet_id     = "${element(aws_subnet.public.*.id,count.index)}"
 
   vpc_security_group_ids = [
--- terraform/instances-keypairs.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "main" {
-  key_name   = "${var.environment_name}"
-  public_key = "${file("../ssh_keypair/id_rsa.pub")}"
-}
