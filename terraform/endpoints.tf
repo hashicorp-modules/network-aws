f26cc22d85b4255097f090ba41c0780c56aec8dd
Cameron Stokes <cameron@cameronstokes.com>
2017-03-14 15:10:41 -0500

    Remove old files.

--- terraform/endpoints.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_vpc_endpoint" "private_s3" {
-	vpc_id = "${aws_vpc.vpc.id}"
-	service_name = "com.amazonaws.us-west-2.s3"
-	route_table_ids = [
-		"${aws_route_table.private.*.id}",
-		"${aws_route_table.public.*.id}"
-	]
-}
