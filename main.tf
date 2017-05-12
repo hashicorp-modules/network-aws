43f464de7dfd75c50db11ed347419c1e52e7f419
Dan Brown <roooms@gmail.com>
2017-05-12 09:10:55 -0700

    Pass os_version to fix base image lookup

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ terraform {
 data "aws_availability_zones" "main" {}
 
 module "images-aws" {
-  source = "git@github.com:hashicorp-modules/images-aws.git"
-  os     = "${var.os}"
+  source     = "git@github.com:hashicorp-modules/images-aws.git"
+  os         = "${var.os}"
+  os_version = "${var.os_version}"
 }
