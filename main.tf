ceccc969991b10fc3cc64951f9d68baf51bc1228
Dan Brown <roooms@gmail.com>
2017-05-31 07:48:42 +0100

    Fix against image-aws tag

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
 data "aws_availability_zones" "main" {}
 
 module "images-aws" {
-  source     = "git@github.com:hashicorp-modules/images-aws.git"
+  source     = "git@github.com:hashicorp-modules/images-aws.git?ref=2017-05-26"
   os         = "${var.os}"
   os_version = "${var.os_version}"
 }
